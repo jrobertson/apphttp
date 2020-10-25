e86ed885a507160bae739b3ed3ca4b04198b2b65
James Robertson <james@jamesrobertson.eu>
2020-10-25 18:31:55 +0000

    feature: An index page is automatically generated if it does not already exist.

--- apphttp.gemspec
+++ apphttp.gemspec
@@ -1,9 +1,10 @@
 Gem::Specification.new do |s|
   s.name = 'apphttp'
-  s.version = '0.1.0'
+  s.version = '0.2.0'
   s.summary = 'Makes it trivial to web enable a Ruby project.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/apphttp.rb']
+  s.add_runtime_dependency('kramdown', '~> 2.3', '>=2.3.0')    
   s.signing_key = '../privatekeys/apphttp.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
--- lib/apphttp.rb
+++ lib/apphttp.rb
@@ -6,12 +6,19 @@
 
 require 'socket'
 require 'json'
+require 'c32'
+require 'kramdown'
 
 
 class AppHttp
+  using ColouredText
 
-  def initialize(app, host: '0.0.0.0', port: '9232', debug: false)
+  def initialize(app, host: '0.0.0.0', port: '9232', filepath: '.', 
+                 headings: true, debug: false)
+    
     @app, @host, @port, @debug = app, host, port, debug
+    @filepath, @headings = filepath, headings
+    
   end
 
   def start
@@ -20,11 +27,13 @@ class AppHttp
 
     while (session = server.accept)
 
+
       raw_request = session.gets
       request = raw_request[/.[^\s]+(?= HTTP\/1\.\d)/].strip
 
-      result,content_type = get(request)
-      puts 'content_type: ' + content_type.inspect if @debug
+      puts ('request: ' + request.inspect).debug if @debug
+      result, content_type = get(request)
+      puts ('content_type: ' + content_type.inspect).debug if @debug
 
       if result then
         response = result
@@ -39,12 +48,22 @@ class AppHttp
       session.print "\r\n"
       session.print response
       session.close
+
     end
   end
 
   private
 
   def get(s)
+
+    if s == '/' then
+      
+      fp = File.join(@filepath, 'index.html')      
+      return (File.exists?(fp) ? File.read(fp) : default_index() )
+
+    end
+   
+    return [s.to_s, 'text/plain'] if s =~ /^\/\w+\.\w+/
 
     args = []
 
@@ -53,11 +72,12 @@ class AppHttp
       name, args = *s.split('/')[1..-1]
 
     else
-
+      puts ('s: ' + s.inspect).debug if @debug
       name, raw_params = s[/(?<=^\/).*/].split('?',2)
 
       if raw_params then
 
+        puts ('raw_params: ' + raw_params.inspect).debug if @debug
         raw_pairs = raw_params.split('&')
 
         h = raw_pairs.inject({}) do |r,x|
@@ -86,21 +106,107 @@ class AppHttp
     if @app.respond_to? name.to_sym then
 
       begin
-        r = @app.method(name.to_sym).call(*args)
+        
+        method = @app.method(name.to_sym)
+        
+        if method.arity > 0 and  args.length <= 0 then
+          r = "
+          <form action='#{name}'>
+            <input type='text' name='arg'></input>
+            <input type='submit'/>
+          </form>"
+        else
+          r = method.call(*args)
+        end
+
+        fp = File.join(@filepath, File.basename(name) + '.html')
+
+        content = if File.exists?(fp) then        
+          render_html(fp, r)
+        else
+
+          if @headings then
+            markdown = "
+# #{name.capitalize}          
+
+<output>#{r}</output>
+"
+
+            Kramdown::Document.new(markdown).to_html
+          else
+            
+            r
+          end
+
+
+        end
+
       rescue
-        r = ($!).inspect
+        content = ($!).inspect
       end
 
-      case r.class.to_s
+      case content.class.to_s
       when "String"
-        [r, 'text/plain']
+        media = content.lstrip[0] == '<' ? 'html' : 'plain'
+        [content, 'text/' + media]
       when "Hash"
-        [r.to_json,'application/json']
+        [content.to_json,'application/json']
       else
-        [r.to_s, 'text/plain']
+        [content.to_s, 'text/plain']
       end
     end
 
+  end
+  
+  def default_index()
+    
+    a = (@app.public_methods - Object.public_methods).sort
+    s = a.map {|x| "* [%s](%s)" % [x,x]}.join("\n")
+
+    markdown = "
+<html>
+  <head>
+  <title>#{@app.class.to_s}</title>
+  <style>
+h1 {color: green}
+h2 {color: orange}
+div {height: 60%; overflow-y: auto; width: 200px; float: left}
+  </style>
+  </head>
+<body markdown='1'>
+
+# #{@app.class.to_s} Index
+
+## Public Methods
+
+<div markdown='1'>
+#{s}
+</div>
+<iframe name='i1'></iframe>
+<div style='clear: both' />
+<hr/>
+</body>
+</html>    
+    "    
+    #markdown = s
+    doc = Rexle.new(Kramdown::Document.new(markdown).to_html)
+    
+    doc.root.xpath('body/div/ul/li/a') do |link|
+      link.attributes[:target] = 'i1'
+    end
+    
+    [doc.xml(pretty: true), 'text/html']    
+  end
+  
+  def render_html(fp, s)
+    
+    doc = Rexle.new(File.read fp)
+    e = doc.root.element('//[@class="output"]')    
+    e.text = s
+    
+
+    doc.xml pretty: true    
+    
   end
 
 end
