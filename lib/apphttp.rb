eb104d21504f97815c14f3aafad877dfb4a6966e
James Robertson <james@jamesrobertson.eu>
2020-10-26 22:25:01 +0000

    refactoring: The *get()* functionality has now been moved into the gem apphtml_layer. Makes it easier to maintain projects.

--- lib/apphttp.rb
+++ lib/apphttp.rb
@@ -5,9 +5,7 @@
 # desc: Makes it trivial to web enable a Ruby project
 
 require 'socket'
-require 'json'
-require 'c32'
-require 'kramdown'
+require 'apphtml_layer'
 
 
 class AppHttp
@@ -18,6 +16,8 @@ class AppHttp
     
     @app, @host, @port, @debug = app, host, port, debug
     @filepath, @headings = filepath, headings
+    @ah = AppHtmlLayer.new(app, filepath: filepath, headings: headings, 
+                      debug: debug)
     
   end
 
@@ -55,158 +55,6 @@ class AppHttp
   private
 
   def get(s)
-
-    if s == '/' then
-      
-      fp = File.join(@filepath, 'index.html')      
-      return (File.exists?(fp) ? File.read(fp) : default_index() )
-
-    end
-   
-    return [s.to_s, 'text/plain'] if s =~ /^\/\w+\.\w+/
-
-    args = []
-
-    if s.count('/') > 1 then
-
-      name, args = *s.split('/')[1..-1]
-
-    else
-      puts ('s: ' + s.inspect).debug if @debug
-      name, raw_params = s[/(?<=^\/).*/].split('?',2)
-
-      if raw_params then
-
-        puts ('raw_params: ' + raw_params.inspect).debug if @debug
-        raw_pairs = raw_params.split('&')
-
-        h = raw_pairs.inject({}) do |r,x|
-          key,value = x.split('=',2)
-          r.merge(key.to_sym => value) if value
-        end
-
-        return unless h
-
-        h2 = h.inject({}) do |r,x|
-
-          if x.first.to_s =~ /^arg/ then
-            args << x.last
-          else
-            r.merge!(x.first => x.last)
-          end
-          r
-        end
-
-        args << h2 if h2.any?
-
-      end
-
-    end    
-
-    if @app.respond_to? name.to_sym then
-
-      begin
-        
-        method = @app.method(name.to_sym)
-        
-        if method.arity > 0 and  args.length <= 0 then
-          r = "
-          <form action='#{name}'>
-            <input type='text' name='arg'></input>
-            <input type='submit'/>
-          </form>"
-        else
-          r = method.call(*args)
-        end
-
-        fp = File.join(@filepath, File.basename(name) + '.html')
-
-        content = if File.exists?(fp) then        
-          render_html(fp, r)
-        else
-
-          if @headings then
-            markdown = "
-# #{name.capitalize}          
-
-<output>#{r}</output>
-"
-
-            Kramdown::Document.new(markdown).to_html
-          else
-            
-            r
-          end
-
-
-        end
-
-      rescue
-        content = ($!).inspect
-      end
-
-      case content.class.to_s
-      when "String"
-        media = content.lstrip[0] == '<' ? 'html' : 'plain'
-        [content, 'text/' + media]
-      when "Hash"
-        [content.to_json,'application/json']
-      else
-        [content.to_s, 'text/plain']
-      end
-    end
-
+    @ah.lookup s
   end
-  
-  def default_index()
-    
-    a = (@app.public_methods - Object.public_methods).sort
-    s = a.map {|x| "* [%s](%s)" % [x,x]}.join("\n")
-
-    markdown = "
-<html>
-  <head>
-  <title>#{@app.class.to_s}</title>
-  <style>
-h1 {color: green}
-h2 {color: orange}
-div {height: 60%; overflow-y: auto; width: 200px; float: left}
-  </style>
-  </head>
-<body markdown='1'>
-
-# #{@app.class.to_s} Index
-
-## Public Methods
-
-<div markdown='1'>
-#{s}
-</div>
-<iframe name='i1'></iframe>
-<div style='clear: both' />
-<hr/>
-</body>
-</html>    
-    "    
-    #markdown = s
-    doc = Rexle.new(Kramdown::Document.new(markdown).to_html)
-    
-    doc.root.xpath('body/div/ul/li/a') do |link|
-      link.attributes[:target] = 'i1'
-    end
-    
-    [doc.xml(pretty: true), 'text/html']    
-  end
-  
-  def render_html(fp, s)
-    
-    doc = Rexle.new(File.read fp)
-    e = doc.root.element('//[@class="output"]')    
-    e.text = s
-    
-
-    doc.xml pretty: true    
-    
-  end
-
 end
